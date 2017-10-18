ea4f44c7945b0ff5bccb1fafd72837e760d672b7
Alexander Gurinov <alexander.gurinov@gmail.com>
2017-10-18 20:51:40 +0300

    test for testing

--- docker/tools_test.go
+++ docker/tools_test.go
@@ -1,10 +1,43 @@
 package docker
 
-import "testing"
+import (
+	"net"
+	"regexp"
+	"testing"
+)
 
 func TestGetIPByTCPURL(t *testing.T) {
-	ip := getIPByTCPURL("")
-	// if ip != nil {
-	//    t.Errorf("Sum was incorrect, got: %d, want: %d.", ip, nil)
-	// }
+	if ip := getIPByTCPURL(""); ip != nil {
+		t.Errorf("Expecting %d, got: %d", nil, ip)
+	}
+
+	if ip := getIPByTCPURL("tcp://golang.org"); ip != nil {
+		t.Errorf("Expecting %d, got: %d", nil, ip)
+	}
+
+	if ip := getIPByTCPURL("tcp://192.168.99.101:2376"); ip == nil || !ip.Equal(net.ParseIP("192.168.99.101")) {
+		t.Errorf("Expecting %d, got: %d", net.ParseIP("192.168.99.101"), ip)
+	}
+}
+
+func TestSubdomainRegexExpression(t *testing.T) {
+	validDomains := []string{
+		"foo", "foo-bar",
+		"f-------------------------------------------------------------r", // max length check
+	}
+	for _, d := range validDomains {
+		if matched, _ := regexp.MatchString("^"+subdomainRegexExpression+"$", d); matched == false {
+			t.Errorf("%d is valid domain", d)
+		}
+	}
+
+	invalidDomains := []string{
+		"", "-foo", "foo-bar-", "foo-bar,baz",
+		"f--------------------------------------------------------------r", // max length check
+	}
+	for _, d := range invalidDomains {
+		if matched, _ := regexp.MatchString("^"+subdomainRegexExpression+"$", d); matched == true {
+			t.Errorf("%d is invalid domain", d)
+		}
+	}
 }
